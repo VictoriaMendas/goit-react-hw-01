1dc38edba2771863314747de706c3fcd337499fe
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2024-07-26 10:30:20 +0200

    change css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import FriendListItem from '../FriendListItem/FriendListItem';
-
+import css from './FriendList.module.css';
 export default function FriendsList({ friends }) {
   return (
     <ul>
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,12 +2,14 @@ import css from './FriendListItem.module.css';
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <div>
-      <img src={avatar} alt={name} width="48" />
-      <p>{name}</p>
-      <p className={isOnline ? css.on : css.off}>
-        {isOnline ? 'Online' : 'Offline'}
-      </p>
+    <div className={css.cards}>
+      <div className={css.contentList}>
+        <img src={avatar} alt={name} width="48" />
+        <p className={css.title}>{name}</p>
+        <p className={isOnline ? css.on : css.off}>
+          {isOnline ? 'Online' : 'Offline'}
+        </p>
+      </div>
     </div>
   );
 }
